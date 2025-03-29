fc1a0ec8a0591fddfad0d37b7268f6d1d9a6865c
이유석 <yusuk199@naver.com>
2025-03-29 15:13:24 +0900

    수정완료

--- sturct+link/2023082524_Struct_Student.c
+++ sturct+link/2023082524_Struct_Student.c
@@ -1,64 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define MAX_STUDENTS 20
+#define MAX_STUDENT 20
 #define MAX_NAME 25
 
-struct Student
-{
+typedef struct{
     int id;
     char name[MAX_NAME];
     int score;
-};
+}student;
 
-void inputStudents(struct Student students[], int n){
+void inputstudent(student students[], int n){
     for(int i=0;i<n;i++){
-        printf("write %d student info.\n",(i+1));
+        printf("\n ==enter info for student %d ==\n",i+1);
 
-        printf("ID:\n");
+        printf("id:");
         scanf("%d",&students[i].id);
-        printf("Name:\n");
-        scanf("%s",&students[i].name);
-        printf("Score:\n");
+
+        printf("name:");
+        scanf("%s",students[i].name);
+
+        printf("score:");
         scanf("%d",&students[i].score);
-        
-
 
     }
 }
 
-void printStudents(struct Student students[], int n){
-    for(int i = 0; i < n; i++){
-        printf("\nStudent %d\n", i+1);
-        printf("ID: %d\n", students[i].id);
-        printf("Name: %s\n", students[i].name);
-        printf("Score: %d\n", students[i].score);
-    }
-}
-
-
-void searchStudent(struct Student students[], int n, int id){
+void printstudent(student students[], int n){
+    printf("\n===Student List===\n");
     for(int i=0;i<n;i++){
-        if(students[i].id==id){
-            printf("Student Found:");
-            printf("ID:%d",students[i].id);
-            printf("Name:%s",students[i].name);
-            printf("Score:%d",students[i].score);
+        printf("Stuent %d\n",i+1);
+        printf("id    :%d\n",students[i].id);
+        printf("name  :%s\n",students[i].name);
+        printf("score :%d\n",students[i].score);
+    }
+}
 
-
+void searchstudent(student students[],int n, int target){
+    int found=0;
+    for(int i=0;i<n;i++){
+        if(students[i].id==target){
+            printf("\n=== Student Found ===\n");
+            printf("ID    : %d\n", students[i].id);
+            printf("Name  : %s\n", students[i].name);
+            printf("Score : %d\n", students[i].score);
+            found=1;
+            break;
         }
     }
+    if(found==0)printf("the student id is wrong");
 }
 
-void sortStudentsByScore(struct Student students[], int n){
-    struct Student temp;
-    for (int i = 0; i < n-1; i++){
-        for(int j = 0; j< n - i -1; j++){
-            if(students[j].score < students[j+1].score){
-                temp = students[j];
-                students[j] = students[j+1];
-                students[j+1] = temp;
+void sortstudent(student students[], int n){
+    student temp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(students[j].score>students[j+1].score){
+                temp=students[j];
+                students[j]=students[j+1];
+                students[j+1]=temp;
             }
         }
     }
@@ -66,35 +67,28 @@ void sortStudentsByScore(struct Student students[], int n){
 
 int main(){
     int n;
-    printf("Enter the number of students (max %d): ", MAX_STUDENTS);
+    printf("enter number of student (max %d):",MAX_STUDENT);
     scanf("%d",&n);
-    if(n>MAX_STUDENTS){
-        printf("학생수가 최대수를 초과하여 종료합니다.");
-        return 1;
-    }
-    struct Student students[n];
-    
-    
-    
 
-    inputStudents(students, n);
-    sortStudentsByScore(students, n);
-    printf("\nStudents sorted by scores: \n");
-    printStudents(students, n);
+    if(n<=0 || n>MAX_STUDENT){printf("invalid number of student"); return 0;}
 
-    int searchID;
+    student students[n];
+
+    inputstudent(students,n);
+    sortstudent(students,n);
+    printstudent(students,n);
+
+    int searchid;
     while (1)
     {
-        printf("\nEnter the ID of the student you want to search 9Enter -1 to exit): ");
-        scanf("%d", &searchID);
-        if (searchID == -1)
-        {
-            break;
-        }
-        searchStudent(students, n, searchID);
-        
-    }
+        printf("enter student id to search(-1 to exit):");
+        scanf("%d",&searchid);
 
+        if(searchid==-1) break;
+
+        searchstudent(students,n,searchid);
+    }
+    
     return 0;
     
 }
